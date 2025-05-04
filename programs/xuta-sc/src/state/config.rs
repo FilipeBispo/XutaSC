c822cb10ec7f46df33d198e958764b228ea59190
rute-sophie <rute.c.lemos@gmail.com>
2025-05-05 00:28:46 +0100

    Init instruction implementation and Config account initialization

--- programs/xuta-sc/src/state/config.rs
+++ programs/xuta-sc/src/state/config.rs
@@ -2,9 +2,9 @@ use anchor_lang::prelude::*;
 
 #[derive(InitSpace)]
 #[account]
-pub struct Admin {
+pub struct Config {
     pub authority: Pubkey,
-    pub institutionAuthority: Pubkey,
+    pub institution_authority: Pubkey,
     pub fee_pre: u16,
     pub fee_pos: u16,
     pub bump: u8, // For PDA bump seed
