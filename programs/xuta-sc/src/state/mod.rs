01f2cbf516af949b77fee7ad4bd28c4476c91e7b
FilipeBispo <fmbispo@gmail.com>
2025-05-09 16:42:39 +0100

    CHANGING error

--- programs/xuta-sc/src/state/mod.rs
+++ programs/xuta-sc/src/state/mod.rs
@@ -2,12 +2,10 @@ pub mod campaign;
 pub mod enums;
 pub mod config;
 pub mod receipt;
-pub mod errors;
 pub mod institution;
 
 pub use campaign::*;
 pub use enums::*;
 pub use config::*;
 pub use receipt::*;
-pub use errors::*;
 pub use institution::*;
