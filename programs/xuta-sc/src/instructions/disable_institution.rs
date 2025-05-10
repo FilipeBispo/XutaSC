18933464df5c338785787c06ec40902f49587d38
rute-sophie <rute.c.lemos@gmail.com>
2025-05-10 12:24:20 +0100

    disable institution added

--- programs/xuta-sc/src/instructions/disable_institution.rs
+++ programs/xuta-sc/src/instructions/disable_institution.rs
@@ -1,10 +1,34 @@
 use anchor_lang::prelude::*;
+use crate::{state::{Config, Institution}, error::CustomError};
 
+pub fn disable_institution(ctx: Context<DisableInstitution>) -> Result<()> {
+    // Check if there are any active campaigns
+    require!(
+        !ctx.accounts.institution.has_active_campaigns,
+        CustomError::InstitutionHasActiveCampaigns
+    );
 
-pub fn disable_institution(_ctx: Context<DisableInstitution>) -> Result<()> {
-    // implementation to be made
+    // Set the disabled flag to true
+    ctx.accounts.institution.disabled = true;
     Ok(())
 }
 
 #[derive(Accounts)]
-pub struct DisableInstitution {}
+pub struct DisableInstitution<'info> {
+    #[account(mut)]
+    pub institution_authority: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"institution".as_ref(), institution.name.as_ref()],
+        bump = institution.bump,
+    )]
+    pub institution: Account<'info, Institution>,
+
+    #[account(
+        seeds = [b"config".as_ref()],
+        bump = config.bump,
+        has_one = institution_authority @ CustomError::Unauthorized,
+    )]
+    pub config: Account<'info, Config>,
+}
--- programs/xuta-sc/src/state/institution.rs
+++ programs/xuta-sc/src/state/institution.rs
@@ -8,5 +8,7 @@ pub struct Institution {
     pub name: String,
     #[max_len(100)]
     pub contract: String, // might be an NFT if we have time
+    pub disabled: bool,
+    pub has_active_campaigns: bool,
     pub bump: u8, // For PDA bump seed
 }
