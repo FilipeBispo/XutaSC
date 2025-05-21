3091bb4522c97c294e2b1a2765464e493f2795a8
FilipeBispo <fmbispo@gmail.com>
2025-05-21 22:58:09 +0100

    test needs fixing (tommorrow)

--- tests/xuta-sc.ts
+++ tests/xuta-sc.ts
@@ -4,7 +4,7 @@ import { XutaSc } from "../target/types/xuta_sc";
 import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 import { BN } from "bn.js";
 import { createMint, getOrCreateAssociatedTokenAccount, getAccount, getAssociatedTokenAddressSync, TOKEN_PROGRAM_ID, mintTo } from "@solana/spl-token";
-import {assert, expect} from 'chai';
+import { assert, expect } from 'chai';
 import { Campaigns } from "../Xuta-fe/src/containers";
 
 describe("xuta-sc", () => {
@@ -24,6 +24,7 @@ describe("xuta-sc", () => {
   //let mintPlayer: PublicKey;
   //let mintQuote: PublicKey;
   let userQuoteAccount;
+  let userPlayerTokenAccount;
   let receipt: PublicKey;
   let connection: Connection;
 
@@ -39,6 +40,10 @@ describe("xuta-sc", () => {
     console.log("Admin pk= ", admin.publicKey);
     connection = anchor.getProvider().connection;
 
+    let airdropSignature = await connection.requestAirdrop(admin.publicKey, LAMPORTS_PER_SOL * 10);
+    await connection.confirmTransaction(airdropSignature);
+
+
     const sigs = await Promise.all([
 
       connection.requestAirdrop(admin.publicKey, LAMPORTS_PER_SOL * 10),
@@ -50,10 +55,10 @@ describe("xuta-sc", () => {
       connection.confirmTransaction(sigs[1]),
       connection.confirmTransaction(sigs[2])
     ]);
-    
+
     await createMint(connection, admin, admin.publicKey, null, 6, mintQuote);
 
-        // user = the Keypair who wants to buy tokens
+    // user = the Keypair who wants to buy tokens
     userQuoteAccount = await getOrCreateAssociatedTokenAccount(
       connection,
       user, // payer for account creation
@@ -65,17 +70,18 @@ describe("xuta-sc", () => {
       TOKEN_PROGRAM_ID
     );
 
-    await mintTo(connection, admin, mintQuote.publicKey, userQuoteAccount.address, admin, 100000 );
+    await mintTo(connection, admin, mintQuote.publicKey, userQuoteAccount.address, admin, 100000);
+
   });
 
 
   it("Initialize config!", async () => {
     // Add your test here.
     const tx = await program.methods
-    .init()
-    .accounts({authority: admin.publicKey})
-    .signers([admin])
-    .rpcAndKeys();
+      .init()
+      .accounts({ authority: admin.publicKey })
+      .signers([admin])
+      .rpcAndKeys();
 
     let cfgPk = tx.pubkeys.config;
     const cfg = await program.account.config.fetch(cfgPk);
@@ -91,14 +97,14 @@ describe("xuta-sc", () => {
   it("inititialize institution!", async () => {
 
     const tx = await program.methods
-    .initInstitution("Soccer Stars","URL_PDF","URL_IMAGE","Soccer star is the best")
-    .accountsPartial({
-      institutionAuthority: admin.publicKey,
-      newInstitutionAuthority: institutionAdmin.publicKey,
+      .initInstitution("Soccer Stars", "URL_PDF", "URL_IMAGE", "Soccer star is the best")
+      .accountsPartial({
+        institutionAuthority: admin.publicKey,
+        newInstitutionAuthority: institutionAdmin.publicKey,
 
-    })
-    .signers([admin])
-    .rpcAndKeys();
+      })
+      .signers([admin])
+      .rpcAndKeys();
 
     console.log(tx);
     console.log("Your transaction signature", tx.signature);
@@ -108,7 +114,7 @@ describe("xuta-sc", () => {
   it("sets new authority!", async () => {
     // Create a new keypair for the new authority
     const newAuthority = Keypair.generate();
-    
+
     // Request airdrop for the new authority
     const sig = await connection.requestAirdrop(newAuthority.publicKey, LAMPORTS_PER_SOL * 10);
     await connection.confirmTransaction(sig);
@@ -133,28 +139,27 @@ describe("xuta-sc", () => {
     admin = newAuthority;
   });
 
-    it("creates a campaign!", async () => {
+  it("creates a campaign!", async () => {
 
     const tx = await program.methods
-    .createCampaign( "Ronaldo", 
-                    "URL_contract", 
-                    "URL_Image", 
-                    "Invest in Ronaldo", 
-                    10, 
-                    new BN(1000), 
-                    new BN(1747250749), 
-                    new BN(1767250740)
-                  )
-    .accountsPartial({
-
-      mintPlayer: mintPlayer.publicKey,
-      mintQuote: mintQuote.publicKey,
-      authority: institutionAdmin.publicKey,
-      config: config,
-      institution: institutionAccount,
-    })
-    .signers([institutionAdmin,mintPlayer])
-    .rpcAndKeys();
+      .createCampaign("Ronaldo",
+        "URL_contract",
+        "URL_Image",
+        "Invest in Ronaldo",
+        10,
+        new BN(1000),
+        new BN(1747250749),
+        new BN(1767250740)
+      )
+      .accountsPartial({
+        mintPlayer: mintPlayer.publicKey,
+        mintQuote: mintQuote.publicKey,
+        authority: institutionAdmin.publicKey,
+        config: config,
+        institution: institutionAccount,
+      })
+      .signers([institutionAdmin, mintPlayer])
+      .rpcAndKeys();
 
     console.log(tx);
     console.log("Your transaction signature", tx.signature);
@@ -163,26 +168,55 @@ describe("xuta-sc", () => {
 
   it("buying tokens!", async () => {
 
+    userPlayerTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      user,                    // payer (also ATA owner here)
+      mintPlayer.publicKey,    // the new token mint
+      user.publicKey           // ATA owner
+    );
+    
     console.log(userQuoteAccount);
     const tx = await program.methods
-    .buyToken( 
-      new BN(100), 
-      1
-    )
-    .accountsPartial({
-      user: user.publicKey,
-      mintQuote: mintQuote.publicKey,
-      config: config,
-      campaign: campaignAccount,
-      userQuoteAta: userQuoteAccount.address,
+      .buyToken(
+        new BN(100),
+        1
+      )
+      .accountsPartial({
+        user: user.publicKey,
+        mintQuote: mintQuote.publicKey,
+        config: config,
+        campaign: campaignAccount,
+        userQuoteAta: userQuoteAccount.address,
 
-    })
-    .signers([user])
-    .rpcAndKeys();
+      })
+      .signers([user])
+      .rpcAndKeys();
 
     console.log(tx);
     console.log("Your transaction signature", tx.signature);
     receipt = tx.pubkeys.receipt;
   });
 
+  it("redeem tokens!", async () => {
+
+    console.log("User's ATA for mintPlayer:", userPlayerTokenAccount.address);
+    const tx = await program.methods
+      .redeemToken(
+      )
+      .accountsPartial({
+        user: user.publicKey,
+        mintPlayer: mintPlayer.publicKey,
+        campaign: campaignAccount,
+        userTokenAccount: userPlayerTokenAccount.address,
+        receipt: receipt,
+      })
+      .signers([user])
+      .rpcAndKeys();
+
+    const tokenAccountInfo = await getAccount(connection, userPlayerTokenAccount.address);
+    console.log("Balance:", tokenAccountInfo.amount.toString());
+
+    console.log(tx);
+    console.log("Your transaction signature", tx.signature);
+  });
 });
